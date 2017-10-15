6851e73781c4e26062f0dc6ae1040d56eff7b7dc
Dellaire <henry.borasch@freenet.de>
2017-10-15 20:36:27 +0200

    added CORS configuration

--- src/main/java/de/jet/tournamentmaker/gateway/GatewayApplication.java
+++ src/main/java/de/jet/tournamentmaker/gateway/GatewayApplication.java
@@ -3,6 +3,10 @@ package de.jet.tournamentmaker.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -11,5 +15,20 @@ public class GatewayApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter()
+	{
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsFilter(source);
 	}
 }
