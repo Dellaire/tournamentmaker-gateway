9f02ff125b357ae2d49c6240a11c784dcd043ccb
Dellaire <henry.borasch@freenet.de>
2017-12-30 15:18:30 +0100

    added property to disable CSRF protection for specific paths

--- src/main/java/de/jet/tournamentmaker/gateway/SecurityConfig.java
+++ src/main/java/de/jet/tournamentmaker/gateway/SecurityConfig.java
@@ -1,6 +1,10 @@
 package de.jet.tournamentmaker.gateway;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +13,9 @@ import org.springframework.security.web.csrf.CsrfFilter;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
+	@Value("${csrf.ignore.paths}")
+	private String csrfIgnorePaths = "";
+
 	@Autowired
 	private CsrfTokenResponseHeaderBindingFilter csrfTokenResponseHeaderBindingFilter;
 
@@ -17,7 +24,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 	{
 		http.authorizeRequests().antMatchers("/").authenticated().and().formLogin();
 
-		http.csrf().ignoringAntMatchers("/matchmaking/**");
+		if (!csrfIgnorePaths.isEmpty())
+		{
+			List<String> pathsToIgnore = Arrays.asList(csrfIgnorePaths.split(","));
+			pathsToIgnore.forEach(path ->
+			{
+				try
+				{
+					http.csrf().ignoringAntMatchers(path);
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			});
+		}
+
 		http.addFilterAfter(csrfTokenResponseHeaderBindingFilter, CsrfFilter.class);
 	}
 }
