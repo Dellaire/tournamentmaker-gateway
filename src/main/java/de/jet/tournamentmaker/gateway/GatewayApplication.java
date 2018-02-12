2a0e9b0e396892cbb76cbea977473324a456b573
Dellaire <henry.borasch@freenet.de>
2018-02-12 22:13:05 +0100

    extended authorization to any path

--- src/main/java/de/jet/tournamentmaker/gateway/GatewayApplication.java
+++ src/main/java/de/jet/tournamentmaker/gateway/GatewayApplication.java
@@ -3,7 +3,6 @@ package de.jet.tournamentmaker.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
--- src/main/java/de/jet/tournamentmaker/gateway/SecurityConfig.java
+++ src/main/java/de/jet/tournamentmaker/gateway/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
-		http.authorizeRequests().antMatchers("/").authenticated().and().formLogin();
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin();
 
 		if (!csrfIgnorePaths.isEmpty())
 		{
